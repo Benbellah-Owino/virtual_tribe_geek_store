40b62052308f8a16443bb57adb08e27bc2631b0d
Benbellah-Owino <owinobenbellah@gmail.com>
2024-11-07 11:50:49 +0300

    MINOR:-> Commenting out future code

--- backend/src/file_upload/large_file.rs
+++ backend/src/file_upload/large_file.rs
@@ -1,25 +1,25 @@
-pub async fn from_field_big_file(mut field: Field<'_>, destination: String) {
-        eprintln!("{:#?}", &field);
-        let content_type = field.content_type().unwrap().to_string();
-        let name = field.file_name().unwrap().to_string();
-        let mut count = 0 as f32;
-        let pow = usize::pow(2, 20) as f32;
-        println!("{pow}");
-        let dest = gen_file_name(destination, &name, &content_type).unwrap();
-        while let Some(chunk) = field.chunk().await.unwrap() {
-            let len =  chunk.len() as f32;
-            let mbs = len / pow;
-            count = count + mbs;
+// pub async fn from_field_big_file(mut field: Field<'_>, destination: String) {
+//         eprintln!("{:#?}", &field);
+//         let content_type = field.content_type().unwrap().to_string();
+//         let name = field.file_name().unwrap().to_string();
+//         let mut count = 0 as f32;
+//         let pow = usize::pow(2, 20) as f32;
+//         println!("{pow}");
+//         let dest = gen_file_name(destination, &name, &content_type).unwrap();
+//         while let Some(chunk) = field.chunk().await.unwrap() {
+//             let len =  chunk.len() as f32;
+//             let mbs = len / pow;
+//             count = count + mbs;
 
-            println!(
-                "received {}b ({}mb) , total is {}mb", 
-                chunk.len(),
-                mbs,
-                count
-            );
+//             println!(
+//                 "received {}b ({}mb) , total is {}mb", 
+//                 chunk.len(),
+//                 mbs,
+//                 count
+//             );
             
-            let _ = append_to_disk((&dest, chunk));
-        }
-        println!("File \"{}.{}\" sized {}mbs is saved to disk", name, content_type,count);
-    }
+//             let _ = append_to_disk((&dest, chunk));
+//         }
+//         println!("File \"{}.{}\" sized {}mbs is saved to disk", name, content_type,count);
+//     }
 
