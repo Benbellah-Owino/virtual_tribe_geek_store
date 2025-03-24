542590c7627514c221ac4bbe6b369235672bf331
Benbellah-Owino <owinobenbellah@gmail.com>
2025-03-24 14:41:35 +0300

    ADD: Struct that returns only database ID

--- backend/src/helpers/db.rs
+++ backend/src/helpers/db.rs
@@ -1,5 +1,10 @@
+use serde::{Serialize, Deserialize};
 use surrealdb::sql::Thing;
 
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct SurrealID {
+    pub id: Thing,
+}
 pub fn thing_from_string(id: String) -> Thing {
     let id_string: Vec<&str> = id.split(':').collect();
     // Thing {
