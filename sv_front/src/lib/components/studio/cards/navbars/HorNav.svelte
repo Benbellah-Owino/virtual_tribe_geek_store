d8041c34569bbc1dcef8c84f9754c35b2b8ee86d
Benbellah-Owino <owinobenbellah@gmail.com>
2025-08-18 20:18:24 +0300

    ADD<FE>:-> A dynamic link that shows user option to log in or out depending on the login status

--- sv_front/src/lib/components/studio/cards/navbars/HorNav.svelte
+++ sv_front/src/lib/components/studio/cards/navbars/HorNav.svelte
@@ -1,3 +1,16 @@
+<script>
+	import { onMount } from "svelte";
+
+    let isLoggedIn = $state(false);
+    onMount(async()=>{
+        let cookie =await window.cookieStore.get("auth_token")
+        console.log(cookie)
+        isLoggedIn = cookie ? true : false;
+    })
+
+
+</script>
+
 <nav class="secondary_borderb min-h-20 p-2 flex justify-between items-center ">
     <div class="logo">
         <h1 class="text text-6xl font-bold">VRT</h1>
@@ -5,7 +18,11 @@
     <ul class="links flex justify-around items-center text-sm md:text-base">
         <li><a href="/content/comics/" class="nav_link underline">Comics</a></li>
         <li><a href="/user/creator/" class="nav_link underline">My Page</a></li>
-        <li><a href="/user/logout" class="nav_link underline">Logout</a></li>
+        {#if isLoggedIn}
+            <li><a href="/user/logout" class="nav_link underline">Logout</a></li>
+        {:else}
+            <li><a href="/user/login" class="nav_link underline">Login</a></li>
+        {/if}
     </ul>
 </nav>
 
